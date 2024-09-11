435d3f291bc3b4e9c223c78173e771246c71edb6
williamjamesjanohan <williamjamesjanohan1997@gmail.com>
2024-09-11 21:20:09 +0800

    fixed listview issue

---
+++ ToDoList/app/src/main/java/com/example/listview/adapter/ItemAdapter.java
@@ -0,0 +1,70 @@
+package com.example.listview.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.listview.R;
+import com.example.listview.model.Item;
+
+import java.util.List;
+
+public class ItemAdapter extends BaseAdapter {
+    private Context context;
+    private List<Item> items;
+
+    public ItemAdapter(Context context, List<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+            holder = new ViewHolder();
+            holder.imageView = convertView.findViewById(R.id.imageView);
+            holder.textView = convertView.findViewById(R.id.textView);
+            holder.checkBox = convertView.findViewById(R.id.checkBox);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Item item = items.get(position);
+
+        holder.imageView.setImageResource(item.getImageResId());
+        holder.textView.setText(item.getText());
+        holder.checkBox.setChecked(item.isChecked());
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        ImageView imageView;
+        TextView textView;
+        CheckBox checkBox;
+    }
+}
